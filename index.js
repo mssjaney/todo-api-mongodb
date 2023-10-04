bb19ad8cfe18ccd101f9d964f5d86572f65aeadf
mssjaney <janey.webdev@gmail.com>
2023-10-04 16:49:01 +0200

    added CRUD routes for mongodb

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ let express = require('express'),
     app = express(),
     port = 27017 || 3000;
 
+let bodyParser = require('body-parser');
 let todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.send('HELLO FROM ROOT');
